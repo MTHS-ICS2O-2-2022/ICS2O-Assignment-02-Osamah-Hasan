08830f97214fe61c22e91337cf4fe257579b2abe
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-03-27 16:32:17 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Osamah Hasan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Osamah Hasan
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of Triangle.
+ */
+function calculate () {
+  // input
+  const H = parseInt(document.getElementById('H of Triangle').value)
+  const B = parseInt(document.getElementById('B of Triangle').value)
+
+  // process
+  const area = H * B
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
